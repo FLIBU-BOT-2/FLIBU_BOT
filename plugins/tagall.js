90e7551e7c06e946e06d8770e9f9f6c2b74eeaae
FLIBU-BOT-2 <161664729+FLIBU-BOT-2@users.noreply.github.com>
2024-02-29 22:25:13 +0100

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`*الموضوع:*\n${text ? `${text}\n` : ''}\n'*قمت بي اشارة اليك لي تعرف الموضوع*
+    m.reply(`*الموضوع:*\n${text ? `${text}\n` : ''}\n*قمت بي اشارة اليك لي تعرف الموضوع*
     })
 }
 
