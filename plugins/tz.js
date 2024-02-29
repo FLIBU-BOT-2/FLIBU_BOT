7a00891b7a1e9f91e92b38c3114fca81d6dd0728
FLIBU-BOT-2 <161664729+FLIBU-BOT-2@users.noreply.github.com>
2024-02-29 20:52:01 +0100

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -4,12 +4,12 @@ const handler = async (m, {conn}) => {
 
   const tzAF = moment().tz('Africa/Casablanca').format('DD/MM HH:mm');
   await conn.sendMessage(m.chat, {text: `\`\`\`
-الوقت الحالي في المغرب هو :
+الـوقت الـحـالـي فـي الـمـغـرب هـو :
 
 
 ▢ morocco     : ${tzAF}
   ${String.fromCharCode(8206).repeat(850)}
-  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+  ▢ instagram.com/flibu_gaming`}, {quoted: m});
   };
 handler.help = ["tz"]
 handler.tags = ["infobot"]
